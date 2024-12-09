316fb74a00b1e5680961369fd10dc78608911ece
Sahil yadav <sahil9082811@gmail.com>
2024-12-09 06:30:22 -0600

    Adding recursion method in js

---
+++ reverse/reverse.js
@@ -0,0 +1,12 @@
+// const data = [1,2,3,4,5];
+//! BUILD IN METHOD
+// console.log(data.reverse(), 'data.reverse()');
+
+//! USING RECURSION METHOD
+const recursionMethod = (data, start, end) =>{
+    console.log(data, "data");
+    
+}
+
+const data = [1,2,3,4,5];
+recursionMethod(data, start, end)
