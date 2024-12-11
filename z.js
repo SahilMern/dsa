f742835d140aa17ac186fe2c0e5d0c97c9d7caf7
Sahil yadav <sahil9082811@gmail.com>
2024-12-11 05:44:13 -0600

    findIntersection

--- z.js
+++ z.js
@@ -1,10 +1,15 @@
-function findMissingNumber(arr) {
-    let n = arr.length + 1;
-    let total = (n * (n + 1)) / 2;
-    console.log(total);
-    
-    let sum = arr.reduce((acc, num) => acc + num, 0);
-    return total - sum;
-}
+const array1 =[1,2,3, 4]
+const array2 = [5,6,7,8]
+const array3 =[]
 
-console.log(findMissingNumber([1,2,3,9]));
+for (let index = 0; index < array1.length; index++) {
+    console.log(array1[index]);
+    array3[index] = array1[index]
+}
+console.log(array3, "array3");
+
+for (let index = 0; index < array2.length; index++) {
+    console.log(array2[index]);
+    array3[array3.length] = array2[index]
+}
+console.log(array3, "array3");
