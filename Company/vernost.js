c3dd9821ddc62abb5438b57e9ca38a63206256eb
Sahil yadav <sahil9082811@gmail.com>
2024-12-11 04:12:50 -0600

    vernost company

--- Company/vernost.js
+++ Company/vernost.js
@@ -1,5 +1,10 @@
-const array = [1,2,2,3,4,5,4]
-for(let i=0; i<array.length; i++){
-    console.log(array[i]);
-    
+const array = [1,2,2,3,4,5,4];
+let removeDuplicate = [];
+for (let i = 0; i < array.length; i++) {
+    if (!removeDuplicate.includes(array[i])) {
+        removeDuplicate.push(array[i]);
+    }
 }
+console.log(removeDuplicate);
+
+console.log(new Set([...array]));
