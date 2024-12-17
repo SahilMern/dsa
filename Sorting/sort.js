0f93b1ffb9d888eeac8eb69ff3def9bf4747a9a1
Sahil yadav <sahil9082811@gmail.com>
2024-12-17 06:22:17 -0600

    sorting

--- Sorting/sort.js
+++ Sorting/sort.js
@@ -1,7 +1,9 @@
 function bubbleSort(arr) {
   let n = arr.length;
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - 1 - i; j++) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n ; j++) {
+      console.log(j);
+      
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
