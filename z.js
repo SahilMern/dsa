4c03f0dbc2e0b9e09d2b008442e7477897451179
Sahil yadav <sahil9082811@gmail.com>
2024-12-18 06:30:14 -0600

    adding data

--- z.js
+++ z.js
@@ -1,14 +1,1 @@
-// Longest Substring Without Repeating Characters
-let strObj = {};
-const lengthOfLongestSubstring = (String) => {
-  const data = String.split("");
-  console.log(data, "data");
-
-  for (const char of data) {
-    strObj[char] = (strObj[char] || 0) + 1;
-  }
-
-  console.log(strObj);
-  
-};
-console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3 ("abc")
+// Implement a stack with push, pop, and peek operations using arrays.
