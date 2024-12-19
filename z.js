fce4ebaa112da5e3519c8a4f78675bb134272da6
Sahil yadav <sahil9082811@gmail.com>
2024-12-19 03:59:50 -0600

    section sort

--- z.js
+++ z.js
@@ -1,9 +1,14 @@
-function reverseString(str) {
-  let str = ""
-  for (let index = 0; index < str.length; index++) {
-    console.log(index);
-    
+let str = "";
+
+function reverseString(inputString) {
+  let reversedString = ""; // Initialize a new string to store the reversed string
+
+  // Loop through the input string from the last character to the first
+  for (let index = 0; index < inputString.length; index++) {
+    reversedString = inputString[index] + reversedString; // Add the character to the beginning of reversedString
   }
+
+  return reversedString; // Return the reversed string
 }
 
 console.log(reverseString("Hello")); // Output: "olleH"
