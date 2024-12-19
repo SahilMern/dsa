50f9e5b18eb1e42ab78ba27923a345b8fdc7b001
Sahil yadav <sahil9082811@gmail.com>
2024-12-19 01:40:24 -0600

    Stake uisng classes

--- z.js
+++ z.js
@@ -1,1 +1,19 @@
-// Implement a stack with push, pop, and peek operations using arrays.
+const insertInArray = (array, index, value) => {
+  let newArray = new Array(array.length + 1);
+  for (let i = 0; i < index; i++) {
+    newArray[i] = array[i];
+  }
+
+  newArray[index] = value;
+  for (let i = index; i < array.length; i++) {
+    newArray[i+1] = array[i];
+  }
+
+  console.log(newArray, "newArray");
+  
+};
+
+const array = [1, 2, 3, 4, 5];
+let index = 3;
+let value = 6;
+insertInArray(array, index, value);
