904bb5495ba637c65a5fb64a3b3036c7461c062f
Sahil yadav <sahil9082811@gmail.com>
2024-12-23 05:01:40 -0600

    user data

--- z.js
+++ z.js
@@ -12,3 +12,21 @@ function reverseString(inputString) {
 }
 
 console.log(reverseString("Hello")); // Output: "olleH"
+
+
+
+let str = "";
+
+function reverseString(inputString) {
+  let reversedString = ""; // Initialize a new string to store the reversed string
+
+  // Loop through the input string from the last character to the first
+  for (let index = 0; index < inputString.length; index++) {
+    reversedString = inputString[index] + reversedString; // Add the character to the beginning of reversedString
+  }
+
+  return reversedString; // Return the reversed string
+}
+
+console.log(reverseString("Hello")); // Output: "olleH"
+
