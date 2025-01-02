d92c9b927c42ae44af7b93996f73e9621f4056de
SahilMern <sahil9082811@gmail.com>
2025-01-02 23:13:31 +0530

    practicing dublicate and serach and dlete element in aaray

--- z.js
+++ z.js
@@ -1,40 +1,14 @@
-function mergeSortedArrays(arr1, arr2) {
-    let result = [];  // This will store the merged array
-    let i = 0;        // Pointer for the first array
-    let j = 0;        // Pointer for the second array
-
-    // let count =0
-    // Traverse both arrays and insert the smaller element into the result
-    while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) {
-            result.push(arr1[i]);
-            i++;
-        } else {
-            result.push(arr2[j]);
-            j++;
+const finDublicateInArray = (array) =>{
+    let dublicateElemnt= []
+    console.log(array, "array");
+    for (let index = 0; index < array.length; index++) {
+        if(!dublicateElemnt.includes(array[index])){
+            dublicateElemnt.push(array[index])
+        }else{
+            
         }
     }
+console.log(dublicateElemnt, "Final");
 
-    console.log(result, "resulut");
-    
-    // If there are any remaining elements in arr1, push them
-    while (i < arr1.length) {
-        result.push(arr1[i]);
-        i++;
-    }
-
-    // If there are any remaining elements in arr2, push them
-    while (j < arr2.length) {
-        result.push(arr2[j]);
-        j++;
-    }
-
-    return result;
 }
-
-// Example usage
-let arr1 = [11, 3, 5, 7];
-let arr2 = [2, 4, 6, 8];
-let mergedArray = mergeSortedArrays(arr1, arr2);
-
-console.log(mergedArray);  // Output: [1, 2, 3, 4, 5, 6, 7, 8]
+finDublicateInArray([7,9,8,7,9,8,5,3,1,4,1])
