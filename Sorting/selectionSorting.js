2dc616aabbd66367d0b2aba46f5a5bad5bd15e0b
Sahil yadav <sahil9082811@gmail.com>
2025-01-08 03:54:27 -0600

    selection

--- Sorting/selectionSorting.js
+++ Sorting/selectionSorting.js
@@ -2,16 +2,22 @@ function selectionSort(array) {
     let minid;
     for (let i = 0; i < array.length; i++) {
         minid=i;
+        console.log(minid);
+        
         for (let j = i+1; j < array.length; j++) {
-            console.log(array[j]<array[minid],array[j],array[minid], "Ky aaa raha ahi dekho " );
-            console.log("------------------------------------");
+            console.log(array[minid], "j");
             
-            if(array[j]<array[minid]){
-                console.log("Hey");
-                minid=j
+            if (array[j] < array[minid]) {
+                minid = j; 
             }
         }
+        if (minid !== i) {
+            let temp = array[i];
+            array[i] = array[minid];
+            array[minid] = temp;
+        }
     }
+    return array;
 }
 
 const array = [64, 25, 12, 22, 11];
