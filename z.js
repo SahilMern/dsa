c4bef19a7096f85166f92eecbbf486ab2cd41cb2
Sahil yadav <sahil9082811@gmail.com>
2025-01-09 00:25:48 -0600

    pratice reverse the array in js

--- z.js
+++ z.js
@@ -1,9 +1,13 @@
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-    
+const recursionMethod = (data, start, last) => {
+  console.log(data, "data");
+  if (start > last) {
+    let temp = data[start];
+    data[start] = data[last];
+    data[last] = temp;
+    recursionMethod(data, start + 1, last - 1);
   }
-}
+};
 
-const array = [64, 25, 12, 22, 11];
-console.log("Sorted Array:", selectionSort(array)); // Output: [11, 12, 22, 25, 64]
+
+const data = [1, 2, 3, 4, 5];
+recursionMethod(data, 0, data.length - 1);
