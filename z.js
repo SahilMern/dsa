2dd0a8dd94acac86b7259434a94ef2b0a4f2d1d2
Sahil yadav <sahil9082811@gmail.com>
2025-01-09 02:04:29 -0600

    adding stack an queueu in js

--- z.js
+++ z.js
@@ -1,13 +1,2 @@
-const recursionMethod = (data, start, last) => {
-  console.log(data, "data");
-  if (start > last) {
-    let temp = data[start];
-    data[start] = data[last];
-    data[last] = temp;
-    recursionMethod(data, start + 1, last - 1);
-  }
-};
 
 
-const data = [1, 2, 3, 4, 5];
-recursionMethod(data, 0, data.length - 1);
