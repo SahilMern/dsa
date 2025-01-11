aa79b88836f3bd5dc97e88ac153cb67953c27219
Sahil yadav <sahil9082811@gmail.com>
2025-01-11 01:13:53 -0600

    dsa

--- z.js
+++ z.js
@@ -1,2 +1,24 @@
+class List{
+    constructor(data) {
+        this.head = {
+          value: data,
+          next: null, 
+        };
+        this.tail = this.head;
+        this.size = 1;
+      }
 
+      append(nodeData){
+        let newNode = {
+            value: nodeData,
+            next: null, // The next of the new node will be null
+          };
+          this.tail.next = newNode;
+          this.tail = newNode;
+           this.size++;
+      }
+}
 
+const list = new List(200);
+list.append(20)
+console.log(list);
