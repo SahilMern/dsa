ecbea87ad70812ba4d006215976cd9b74adda5fa
Sahil yadav <sahil9082811@gmail.com>
2025-01-27 05:35:31 -0600

    repating char

--- z.js
+++ z.js
@@ -1,17 +1,5 @@
-const findToSum  =(array, target) => {
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
-            // if(array[i] ===array)
-            // console.log(array[i] + array[j] ===target);
-            if(array[i] + array[j] ===target){
-                return [array[i], array[j]]
-            }
-            
-        }        
-    }
+const repeatedString = (str) => {
+
 }
 
-const nums = [2, 7, 11, 15];
-const target = 18;
-
-console.log(findToSum(nums, target));
+repeatedString("sgagahahaji")
