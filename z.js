55fa9c6773dcfa917109783fdbbafb5d28ea02b1
Sahil yadav <sahil9082811@gmail.com>
2025-01-27 05:39:36 -0600

    count the string '

--- z.js
+++ z.js
@@ -1,5 +1,13 @@
 const repeatedString = (str) => {
 
+  let Obj = {}
+  for(let key of str){
+    console.log(key);
+    Obj[key] = (Obj[key]+0) || 1
+  }
+
+  console.log(Obj);
+  
 }
 
 repeatedString("sgagahahaji")
