7fdb996d55569e832bf1df0dd54600794c7e6276
Sahil yadav <sahil9082811@gmail.com>
2025-02-17 01:34:51 -0600

    changing string to number

--- typeCorresion/index.js
+++ typeCorresion/index.js
@@ -1,5 +1,12 @@
 // console.log(09 “4”);
 console.log(10 + "20");
-
 console.log(9-"4");
+console.log(Number("44"));
+
+const data = 44;
+const changeStringToNumber= String(data)
+console.log(changeStringToNumber, typeof changeStringToNumber);
+
+
+
 
