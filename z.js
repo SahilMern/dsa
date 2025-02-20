6f27bde2483aa9562408ca7405183cf8dff4bf9b
Sahil yadav <sahil9082811@gmail.com>
2025-02-20 06:05:48 -0600

    data

--- z.js
+++ z.js
@@ -15,3 +15,40 @@ console.log(array, "array");
 
 pop()
 console.log(array, "array");
+
+const array = []
+let data = array.length
+
+function push(value){
+    array[data] =value
+    data++
+}
+function pop(){
+    array.length--
+}
+
+push(55);
+push(18);
+console.log(array, "array");
+
+pop()
+console.log(array, "array");
+
+const array = []
+let data = array.length
+
+function push(value){
+    array[data] =value
+    data++
+}
+function pop(){
+    array.length--
+}
+
+push(55);
+push(18);
+console.log(array, "array");
+
+pop()
+console.log(array, "array");
+
