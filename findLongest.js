b0b3716cfa60416412c6f5a30c65a66984702846
Sahil yadav <sahil9082811@gmail.com>
2025-02-26 04:05:40 -0600

    data

--- findLongest.js
+++ findLongest.js
@@ -4,7 +4,7 @@ function lengthOfLongestSubstring(s) {
     let maxLength = 0;
 
     for (let right = 0; right < s.length; right++) {
-        if (map.has(s[right])) {
+        if (map.has([right])) {
             left = Math.max(left, map.get(s[right]) + 1);
         }
         map.set(s[right], right);
