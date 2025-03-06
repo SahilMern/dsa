31ae301a9a46f83d4ef9a5b951bf826e2b909b82
Sahil yadav <sahil9082811@gmail.com>
2025-03-06 04:19:59 -0600

    tricky question

--- tricky/interview1.js
+++ tricky/interview1.js
@@ -8,11 +8,6 @@ const salary = () => {
 }
 salary()
 // var (Implicit Global Variable Assignment)
-const salary = () => {
-    salaryData = 2000;  // Implicit global variable assignment
-    console.log(salaryData, "salary");
-}
-salary();  // Outputs: 2000 salary
-console.log(salaryData);  // Outputs: 2000 (global variable)
+
 
 
