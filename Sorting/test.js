caca135299e2798c98d5f30a1bbd9f42ce102e1f
SahilMern <sahil9082811@gmail.com>
2025-04-06 17:05:54 +0530

    Arranging file directory

--- Sorting/test.js
+++ Sorting/test.js
@@ -1,13 +1,13 @@
-const data = [0,1,15,1,155,88,1,0];
+const array = [0, 1, 15, 1, 155, 88, 1, 0];
 
-for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data.length; j++) {
-        if(data[j]>data[j+1]){
-            let temp = data[j];
-            data[j] = data[j+1]
-            data[j+1]=temp
-        }               
-    }    
+for (let i = 0; i < array.length; i++) {
+  let current = array[i];
+  let j = i - 1;
+  console.log(j);
+  while (j >= 0 && arr[j] > current) {
+    array[j + 1] = arr[j];
+    j--;
+  }
 }
 
-console.log(data); // This will print the sorted array
+console.log(array); // This will print the sorted array
--- anagram.js
+++
@@ -1,9 +0,0 @@
-const anagram = (str1, str2) => {
-    
-    if(str1.length !==str2.length){
-        return "No anagram"
-    }
-
-    return str1.split('').sort().join('') === str2.split('').sort().join('');
-}
-console.log(anagram("slient", "listen"));
